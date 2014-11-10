5ed790cca96708a275a927a5b61ef3a063993b82
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-11-10 15:22:40 +0100

    Add logic-package with a WordPairControl that implements WordPairControlInterface

---
+++ src/solidFlow/src/solidflow/logic/WordPairControl.java
@@ -0,0 +1,47 @@
+package solidflow.logic;
+
+import solidflow.domain.WordPairControlInterface;
+
+public class WordPairControl implements WordPairControlInterface {
+
+    @Override
+    public void add(String question, String answer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getRandomQuestion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkGuess(String question, String quess) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String lookup(String question) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean load(String filename) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean save(String filename) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
