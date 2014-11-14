09ca3ced7a077d73a805ed96d5afa3234088850c
Oliver Lassen <privat.oliver.lassen@gmail.com>
2014-11-14 11:01:34 +0100

    Move main from GUI to Startup.java and set system LAF

--- src/solidFlow/src/solidflow/Startup.java
+++ src/solidFlow/src/solidflow/Startup.java
@@ -1,12 +1,26 @@
 package solidflow;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import solidflow.gui.WordPairGui;
 
 public class Startup {
 
-    
-    public static void main(String[] args) {
-        System.out.println("Working!!");
-        
+    public static void main(String[] args) {        
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Startup.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new WordPairGui().setVisible(true);
+            }
+        });
     }
 
 }
